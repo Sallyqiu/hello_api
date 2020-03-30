e96a321475d954ddcaf82c6b8606fbd20a5e1fae
Sally Qiu <969004929@qq.com>
2020-03-30 17:36:23 +0800

    create test for application loading

---
+++ src/test/java/com/hello/api/HelloApiApplicationSpec.java
@@ -0,0 +1,10 @@
+package com.hello.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApiApplicationSpec {
+    @Test
+    void should_not_throw_any_exception_when_load_context() {}
+}

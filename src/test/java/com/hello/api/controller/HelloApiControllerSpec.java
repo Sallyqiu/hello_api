a197fdfab5f4206c3d4b85b92ac40f9630f194dc
Sally Qiu <969004929@qq.com>
2020-03-30 18:01:21 +0800

    Update a fail test case

--- src/test/java/com/hello/api/controller/HelloApiControllerSpec.java
+++ src/test/java/com/hello/api/controller/HelloApiControllerSpec.java
@@ -1,5 +1,6 @@
 package com.hello.api.controller;
 
+import com.hello.api.model.Survey;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest
@@ -21,5 +22,16 @@ public class HelloApiControllerSpec {
     void should_return_200_code_when_normal_case() throws Exception {
         ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post("/hello"));
         result.andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    void should_return_survey_given_id_3() throws Exception {
+        Survey expectSurvey = new Survey();
+        expectSurvey.id = "3";
+
+        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post("/hello"));
+        result
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.id").value(expectSurvey.id));
     }
 }
